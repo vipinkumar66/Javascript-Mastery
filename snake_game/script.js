1cdcfadefbbc5931b074035d35b577e9c3a7a568
vipin <vipinkumarlamba55@gmail.com>
2023-12-07 23:12:53 +0530

    moving the snake to up at start done

--- snake_game/script.js
+++ snake_game/script.js
@@ -4,7 +4,9 @@ const gameBoard = document.getElementById("game__board");
 // Define game variables
 const gridSize = 20;
 let snake = [{x:10, y:10}] //this basically represents the center of the board
+
 let food = generateFood();
+let direction = "up";
 
 function draw(){
     gameBoard.innerHTML = "";
@@ -35,7 +37,7 @@ function setPosition(element, position){
 }
 
 // Testing Draw Function
-draw()
+// draw()
 
 function drawFood(){
     const foodElement = createGameElement("div", "food");
@@ -43,8 +45,36 @@ function drawFood(){
     gameBoard.appendChild(foodElement);
 }
 
+// Generate Food
 function generateFood(){
     const x = Math.floor(Math.random() * gridSize) +1;
     const y = Math.floor(Math.random() * gridSize) +1;
     return {x,y};
 }
+
+// Moving the snake
+function moveSnake(){
+    const head = {...snake[0]};
+    switch(direction){
+        case "right":
+            head.x++;
+            break
+        case "up":
+            head.y--;
+            break
+        case "down":
+            head.y++;
+            break
+        case "left":
+            head.x--;
+            break
+    }
+    snake.unshift(head);
+    snake.pop();
+}
+
+// Test Moving
+setInterval(() => {
+   moveSnake();
+   draw(); //Draw again new position
+}, 200);
